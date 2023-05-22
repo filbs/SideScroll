25a2fa23c6b9175e09e700e09d6be2ace5aef1a8
Filbert Funay <62603808+filbs@users.noreply.github.com>
2023-05-22 11:10:50 +0700

    First Commit

---
+++ Side Scroll/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    public float jumpForce = 3f;
+
+    private Rigidbody2D rb;
+
+    private SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void SpriteFlip(float horizontalInput)
+    {
+        if (horizontalInput < 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(horizontalInput * speed * Time.deltaTime, 0f, 0f));
+        SpriteFlip(horizontalInput);
+
+        if(Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
